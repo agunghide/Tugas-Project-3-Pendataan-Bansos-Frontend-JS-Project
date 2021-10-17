cc9403dc1e0c714a7e6c401512fec5e195af611c
Agung Hidayat <agunghidayat10@gmail.com>
2021-10-17 21:18:02 +0700

    set pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Tugas-Project-3-Pendataan-Bansos-Frontend-JS-Project/'
+    : '/',
   transpileDependencies: [
     'vuetify'
   ]

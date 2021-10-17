f54ddde0e92a9b580d9f507b45f581f3dc6c4602
Agung Hidayat <agunghidayat10@gmail.com>
2021-10-18 03:12:11 +0700

    setting untuk github pages

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
+
 import router from './router'
+import vuetify from './plugins/vuetify'
 import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
   router,
+  vuetify,
   store,
   render: h => h(App),
   created () {
